5c5a0c8589cad35d5cef951a9bcb2898eb90bd52
Pacheco, King JayJay A. <pachecoking38@gmail.com>
2024-04-15 13:57:29 +0800

    up

---
+++ gigcafe/vendor/botman/driver-web/src/Laravel/views/chat.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <title>GigCafe</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/botman-web-widget/build/assets/css/chat.min.css">
+    <style>
+        /* Custom background color for BotMan Web Widget */
+        .widget-container {
+            background-color: red; /* Change to your desired color */
+        }
+    </style>
+</head>
+<body>
+    
+<script id="botmanWidget" src='https://cdn.jsdelivr.net/npm/botman-web-widget/build/js/chat.js'></script>
+</body>
+</html>
