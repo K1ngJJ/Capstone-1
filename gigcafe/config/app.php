e543ced484e388fcc3f2369ab5795d07989443e9
Pacheco, King JayJay A. <pachecoking38@gmail.com>
2024-09-04 13:33:43 +0800

    up

--- gigcafe/config/app.php
+++ gigcafe/config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Zen'),
+    'name' => env('APP_NAME', 'GiGCafe'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://egigcafe.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
